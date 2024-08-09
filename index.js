88036015781c8a8d8606d1a52e67c832d698307c
[BladrenS] <denis_ka_98@mail.ru>
2024-08-10 00:30:51 +0600

    feat: add full js logic (Sat, Aug 10, 2024 12:30:51 AM)

--- index.js
+++ index.js
@@ -11,7 +11,26 @@ const closeLib = document.querySelector('.close-library');
 const librarySongs = document.querySelector('.library-songs');
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
+const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
+const audio = new Audio();
+let isPlay = false;
+let currentTime = 0;
+let info
 let counter = 0;
+
+function playAudio(path) {
+   audio.src = path;
+   audio.currentTime = currentTime;
+   audio.play();
+   isPlay = true;
+};
+
+function pauseAudio() {
+   currentTime = audio.currentTime;
+   audio.pause();
+   isPlay = false;
+};
 
 async function getInfo() {
    let information = await fetch('./songsInfo.JSON')
@@ -22,6 +41,7 @@ getInfo();
 
 async function generateMain(i) {
    const information = await getInfo();
+   info = information;
    song.innerHTML = information[i].song;
    artist.innerHTML = information[i].artist;
    text.innerHTML = information[i].text;
@@ -45,6 +65,7 @@ async function generateLibrary() {
       div.appendChild(span);
       librarySongs.appendChild(div);
    }
+   audio.volume = 0.3
 }
 generateLibrary();
 
@@ -69,24 +90,172 @@ closeLib.addEventListener('click', () => {
 })
 
 next.addEventListener('click', () => {
-   counter += 1; 
-   if (counter >= document.querySelectorAll('.song').length) counter = 0
+   pauseAudio();
+   counter += 1;
+   currentTime = 0;
+   if (counter >= document.querySelectorAll('.song').length) counter = 0;
    generateMain(counter);
+   playAudio(info[counter].path);
+   isPlay = true;
+   play.classList.add('hide');
+   pause.classList.remove('hide');
+   if (randomBoolean) {
+      let index = Math.floor(Math.random() * ((document.querySelectorAll('.song').length) - 0) + 0);
+      while (index === counter) index = Math.floor(Math.random() * ((document.querySelectorAll('.song').length) - 0) + 0);
+      counter = index;
+      generateMain(counter);
+      playAudio(info[counter].path);
+   }
 })
 
 prev.addEventListener('click', () => {
-   counter -= 1; 
-   if (counter < 0) counter = document.querySelectorAll('.song').length - 1
-   generateMain(counter);
+   currentTime = audio.currentTime;
+   if (currentTime > 3 && isPlay === true) {
+      currentTime = 0;
+      playAudio(info[counter].path);
+      play.classList.add('hide');
+      pause.classList.remove('hide');
+   } else {
+      pauseAudio();
+      counter -= 1;
+      currentTime = 0;
+      if (counter < 0) counter = document.querySelectorAll('.song').length - 1;
+      play.classList.add('hide');
+      pause.classList.remove('hide');
+      generateMain(counter);
+      playAudio(info[counter].path)
+   }
 })
 
 librarySongs.addEventListener('click', (e) => {
    let element = e.target.closest('div')
    if (element.className[element.className.length - 1] !== 's'){
-      counter = Number(element.className[element.className.length - 1]);
+      counter = Number(element.className.split(' ').pop());     
       generateMain(counter);
       library.classList.remove('moved');
       closeLib.classList.remove('show');
-   }     
+   }
+   if (isPlay === true) {
+      pauseAudio();
+      currentTime = 0;
+      playAudio(info[counter].path);
+   }
+   if (isPlay === false) {
+      currentTime = 0;
+      playAudio(info[counter].path);
+      play.classList.add('hide');
+      pause.classList.remove('hide');
+   }
 })
+
+play.addEventListener('click', () => {
+   play.classList.add('hide');
+   pause.classList.remove('hide');
+   playAudio(info[counter].path);
+})
+
+pause.addEventListener('click', () => {
+   pause.classList.add('hide');
+   play.classList.remove('hide');
+   pauseAudio();
+})
+
+audio.addEventListener('loadedmetadata', () => {
+   document.querySelector('.default-time').innerHTML = `${Math.floor(audio.duration / 60)} : ${Math.floor(audio.duration % 60) < 9 ? '0' + Math.floor(audio.duration % 60) : Math.floor(audio.duration % 60)}`;
+})
+
+const volumeSlider = document.querySelector(".volume-slider");
+volumeSlider.addEventListener('click', e => {
+  const sliderWidth = window.getComputedStyle(volumeSlider).width;
+  const newVolume = e.offsetX / parseInt(sliderWidth);
+  audio.volume = newVolume;
+  document.querySelector(".volume-percent").style.width = newVolume * 100 + '%';
+}, false)
+
+const volume = document.querySelector("#volume img");
+const noVolume = document.querySelector("#no-volume");
+volume.addEventListener('click', () => {
+   audio.volume = 0;
+   volume.classList.add('hide');
+   noVolume.classList.remove('hide');
+});
+
+noVolume.addEventListener('click', () => { 
+   audio.volume = 0.5;
+   document.querySelector(".volume-percent").style.width = 50 + '%';
+   noVolume.classList.add('hide');
+   volume.classList.remove('hide');
+});
+
+const repeat = document.querySelector('#rep-white');
+const repeatBlue = document.querySelector('#rep-blue');
+let repeatBoolean = false
+repeat.addEventListener('click', () => {
+   repeat.classList.add('hidden');
+   repeatBlue.classList.remove('hidden');
+   repeatBoolean = true;
+   random.classList.remove('hidden');
+   randomBlue.classList.add('hidden');
+   randomBoolean = false;
+})
+
+repeatBlue.addEventListener('click', () => {
+   repeat.classList.remove('hidden');
+   repeatBlue.classList.add('hidden');
+   repeatBoolean = false;
+})
+
+const random = document.querySelector('#random');
+const randomBlue = document.querySelector('#random-blue');
+let randomBoolean = false;
+random.addEventListener('click', () => {
+   random.classList.add('hidden');
+   randomBlue.classList.remove('hidden');
+   randomBoolean = true;
+   repeat.classList.remove('hidden');
+   repeatBlue.classList.add('hidden');
+   repeatBoolean = false;
+})
+
+randomBlue.addEventListener('click', () => {
+   random.classList.remove('hidden');
+   randomBlue.classList.add('hidden');
+   randomBoolean = false;
+})
+
+const range = document.querySelector('.range')
+range.addEventListener("click", e => {
+  const rangeWidth = window.getComputedStyle(range).width;
+  const timeToSeek = e.offsetX / parseInt(rangeWidth) * audio.duration;
+  audio.currentTime = timeToSeek;
+}, false);
+
+
+setInterval(() => {
+   document.querySelector('.actual-time').innerHTML = `${Math.floor(audio.currentTime / 60)} : ${Math.floor(audio.currentTime % 60) <= 9 ? '0' + Math.floor(audio.currentTime % 60) : Math.floor(audio.currentTime % 60)}`;
+   if (audio.currentTime) document.querySelector('.range').value = audio.currentTime * 100 / audio.duration
+   else document.querySelector('.range').value = 0;
+   if (repeatBoolean) {
+      audio.onended = function() {
+         playAudio(info[counter].path);
+      }
+   } else {   
+      audio.onended = function() {
+         counter += 1;
+         if (counter >= document.querySelectorAll('.song').length) counter = 0
+         generateMain(counter);
+         playAudio(info[counter].path);
+      }
+   }
+   if (randomBoolean) {
+      audio.onended = function() {
+         let index = Math.floor(Math.random() * ((document.querySelectorAll('.song').length) - 0) + 0);
+         while (index === counter) index = Math.floor(Math.random() * ((document.querySelectorAll('.song').length) - 0) + 0);
+         counter = index;
+         generateMain(counter);
+         playAudio(info[counter].path);
+      }
+   }
+}, 100)
+
 
